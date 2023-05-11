79815d82be19c577cb681882f106b226144c3968
Ömer Yusuf SORHUN <omersorgun34@hotmail.com>
2023-05-12 00:14:16 +0300

    şifre güncelleme

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -1,12 +1,18 @@
 import 'package:pharmacy/consts/firebase_consts.dart';
 
 class FireStoreServices {
-
   //get users data
   static getUser(uid) {
     return firestore
         .collection(usersCollection)
         .where("id", isEqualTo: uid)
+        .snapshots();
+  }
+
+  static getProducts(category) {
+    return firestore
+        .collection(productsCollection)
+        .where("p_category", isEqualTo: category)
         .snapshots();
   }
 }
